acd0bbb05dd82bf9faa230a96067836a91e878e1
Gregory <gregory.cobena@gmail.com>
2023-05-21 11:54:19 +0200

    first version of the input form UI part

--- src/app.js
+++ src/app.js
@@ -3,13 +3,32 @@
 angular.module('SimpleFormsApp',[]);
 
 angular.module('SimpleFormsApp')
-.controller('RegistrationController', RegistrationController);
+.controller('CalculationController', CalculationController);
 
-function RegistrationController() {
-  var reg = this;
+CalculationController.$inject = ['$scope'];
+function CalculationController($scope) {
+  var calcCtrl = this;
 
-  reg.submit = function () {
-    reg.completed = true;
+  console.log("init Controller");
+
+  calcCtrl.decl = {
+    salaire: 24000,
+    gardeAlternee: false
+  };
+
+  calcCtrl.arrondi = function(x) {
+    return Math.round(x);
+  }
+
+  console.log("default value done");
+
+  calcCtrl.submit = function () {
+    console.log("submit click");
+    console.log('reg', calcCtrl);
+    console.log('$scope.decl', $scope.decl);
+    calcCtrl.declSubmitted = JSON.parse(JSON.stringify(calcCtrl.decl));
+    console.log('declSubmitted', calcCtrl.declSubmitted);
+    calcCtrl.completed = true;
   };
 }
 
