9f5c1ded2af9c8b9126b4dc119c89d8aa1da65f1
Gregory <gregory.cobena@gmail.com>
2023-05-27 15:46:19 +0200

    move js content to a controller (preparing for routes/views)

---
+++ src/app.module.js
@@ -0,0 +1,7 @@
+(function () {
+
+angular.module('App', ['common']);
+
+console.log("load app module");
+
+})();
---
+++ src/common/calcul-impot/calcul-impot.controller.js
@@ -0,0 +1,46 @@
+(function () {    
+    angular.module('common')
+    .controller('CalculImpotController', CalculImpotController);
+    
+    console.log("load app module");
+    
+    CalculImpotController.$inject = ['$scope', '$filter','ImpotService'];
+    function CalculImpotController($scope, $filter, ImpotService) {
+      var calcCtrl = this;
+    
+      console.log("init Controller");
+    
+      calcCtrl.decl = {
+        salaire: 24000,
+        nbEnfants: '2',
+        gardeAlternee: false
+      };
+    
+      calcCtrl.arrondi = function(x) {
+        return Math.round(x);
+      }
+    
+      console.log("default value done");
+    
+      calcCtrl.submit = function () {
+        calcCtrl.response = undefined;
+    
+        calcCtrl.declSubmitted = JSON.parse(JSON.stringify(calcCtrl.decl));
+        console.log('cloned form: ', calcCtrl.declSubmitted);
+        calcCtrl.completed = true;
+    
+        var impotResultPromise = ImpotService.getImpot(calcCtrl.declSubmitted);
+        impotResultPromise.then(function(responseObj) {
+          console.log("copying object to ctrl: ", responseObj);
+          calcCtrl.response = responseObj;
+        })
+      };
+    
+      calcCtrl.displayNombre = function(x) {
+        var nbStr = $filter('number')(x, 0);
+        return nbStr;
+      }
+    }
+    
+    })();
+    
