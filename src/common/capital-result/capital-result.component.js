f62d8b9618d96ff9a28c3e3c4a4f780456dfc85c
Gregory <gregory.cobena@gmail.com>
2023-06-12 16:26:44 +0200

    first delivery of "Interets Composes", with display of 5y, 10y, 15y, 20y results as a component

---
+++ src/common/capital-result/capital-result.component.js
@@ -0,0 +1,13 @@
+(function () {
+    "use strict";
+    
+    angular.module('common')
+    .component('capitalResult', {
+      templateUrl: 'src/common/capital-result/capital-result.html',
+      bindings: {
+        calcResult: '<'
+      }
+    });
+    
+})();
+    
