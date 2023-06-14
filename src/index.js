8fd5bac47c7e428565e5cd178f8b708ec5e8b12a
Gregory <gregory.cobena@gmail.com>
2023-06-14 10:53:18 +0200

    start using webpack works locally (files generated => local website runs ok) now trying to commit only the manually edited file in git ; and the rest to be generated by some build script (to be defined => not tested yet)

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+import './app.module.js';
+
+import './common/common.module.js';
+import './common/capital-result/capital-result.component.js';
+import './common/calc-items/calc-items.component.js';
+import './common/impot-result/impot-result.component.js';
+import './common/services/optimcredit.service.js';
+import './common/calcul-impot/calcul-impot.controller.js';
+import './common/rente-viagere/rente-viagere.controller.js';
+import './common/taux-endettement/taux-endettement.controller.js';
+import './common/interets-composes/interets-composes.controller.js';
+import './common/calcul-credit/calcul-credit.controller.js';
